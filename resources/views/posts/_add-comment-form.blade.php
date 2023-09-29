426efbc2e402672e7c1e9c1ac05126b665582f0c
Alex Abram <kemper_969@mail.ru>
2023-09-29 22:16:06 +0400

    first

---
+++ resources/views/posts/_add-comment-form.blade.php
@@ -0,0 +1,43 @@
+@auth
+    <x-panel>
+        <form method="POST" action="/posts/{{ $post->slug }}/comments">
+            @csrf
+
+            <header class="flex items-center">
+                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}"
+                     alt=""
+                     width="40"
+                     height="40"
+                     class="rounded-full">
+
+                <h2 class="ml-4">Хотите принять участие?</h2>
+                {{--Want to participate?--}}
+            </header>
+
+            <div class="mt-6">
+                <textarea
+                    name="body"
+                    class="w-full text-sm focus:outline-none focus:ring"
+                    rows="5"
+                    placeholder="Quick, thing of something to say!"
+                    required></textarea>
+
+                @error('body')
+                    <span class="text-xs text-red-500">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                <x-form.button>Отправить</x-form.button>
+                {{--Submit--}}
+            </div>
+        </form>
+    </x-panel>
+@else
+    <p class="font-semibold">
+        <a href="/register" class="hover:underline">Register</a> или
+        {{--or--}}
+        <a href="/login" class="hover:underline">log in</a> чтобы оставить комментарий.
+        {{--to leave a comment.--}}
+    </p>
+@endauth
